1f05c2e06a6a33174163d0ea95e10ed16bbb697d
shivammishra4519 <misra3408@gmail.com>
2024-02-26 15:59:42 +0530

    Your commit message here

--- app.js
+++ app.js
@@ -1,18 +1,8 @@
 const express = require('express');
-const { connectToDB } = require('./db_connection.js');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const userRoute = require('./route/user.js');
-const adminRoute = require('./route/adminRoute.js');
-const loginRoute = require('./route/loginRoute.js');
-const addDeviceRoute = require('./route/addDeviceRoute.js');
-const paymentRoute = require('./route/paymentRoute.js');
-const userCountRoute = require('./route/userCountRoute.js');
-const distributorRoute = require('./route/distributorRoute.js');
-const filterRoute = require('./route/filterRoute.js');
-const { getCsv, getPDF } = require('./export.js');
-
+const { connectToDB } = require('./db_connection.js');
 
 // Load environment variables from .env file
 dotenv.config();
@@ -28,6 +18,17 @@ app.use(cors());
 
 // Use bodyParser middleware with extended true
 app.use(bodyParser.json({ extended: true }));
+
+// Import routes
+const userRoute = require('./route/user.js');
+const adminRoute = require('./route/adminRoute');
+const loginRoute = require('./route/loginRoute');
+const addDeviceRoute = require('./route/addDeviceRoute');
+const paymentRoute = require('./route/paymentRoute');
+const userCountRoute = require('./route/userCountRoute');
+const distributorRoute = require('./route/distributorRoute');
+const filterRoute = require('./route/filterRoute');
+const { getCsv, getPDF } = require('./export');
 
 // Define routes
 app.use('/user', userRoute);
@@ -46,10 +47,25 @@ app.get('/', (req, res) => {
     res.send("Hello, this is your Node.js application!");
 });
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Internal Server Error');
+});
+
 // Configure port
 const port = process.env.PORT || 3000;
 
 // Start the server
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
+});
+
+// Graceful shutdown
+process.on('SIGINT', () => {
+    console.log('Shutting down server...');
+    server.close(() => {
+        console.log('Server shut down successfully');
+        process.exit(0);
+    });
 });
